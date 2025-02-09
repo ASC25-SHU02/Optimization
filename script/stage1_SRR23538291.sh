e6a651f29a9df002f21f25c96f913bf0bdb325d9
lshAlgorithm <lishuhuai_brain@163.com>
2025-02-09 18:24:22 +0800

    revert to the best version. Set timing and checking functiona activate as default

--- script/stage1_SRR23538291.sh
+++ script/stage1_SRR23538291.sh
@@ -88,3 +88,47 @@ run_with_timing 'samtools view -@ 20 \
 	../process/SRR23538291/SRR23538291.mRNA.genome.mapped.sorted.dedup.bam \
 	-O BAM \
 	-o ../process/SRR23538291/SRR23538291.mRNA.genome.mapped.sorted.dedup.filtered.bam'
+
+run_with_timing 'samtools view -e "rlen<100000" \
+	-h ../process/SRR23538291/SRR23538291.mRNA.genome.mapped.sorted.dedup.bam \
+	| hisat-3n-table \
+	-u --alignments - --ref ../ref/Homo_sapiens.GRCh38.dna.primary_assembly.fa \
+	--output-name ../process/SRR23538291/SRR23538291_unfiltered_uniq.tsv \
+	--base-change C,T &
+
+	samtools view -e "rlen<100000" \
+	-h ../process/SRR23538291/SRR23538291.mRNA.genome.mapped.sorted.dedup.bam \
+	| hisat-3n-table \
+	-m --alignments - \
+	--ref ../ref/Homo_sapiens.GRCh38.dna.primary_assembly.fa \
+	--output-name ../process/SRR23538291/SRR23538291_unfiltered_multi.tsv \
+	--base-change C,T &
+
+	samtools view \
+	-e "rlen<100000" \
+	-h ../process/SRR23538291/SRR23538291.mRNA.genome.mapped.sorted.dedup.filtered.bam \
+	| hisat-3n-table \
+	-u --alignments - \
+	--ref ../ref/Homo_sapiens.GRCh38.dna.primary_assembly.fa \
+	--output-name ../process/SRR23538291/SRR23538291_filtered_uniq.tsv\
+	--base-change C,T &
+
+    samtools view \
+	-e "rlen<100000" \
+	-h ../process/SRR23538291/SRR23538291.mRNA.genome.mapped.sorted.dedup.filtered.bam \
+	| hisat-3n-table \
+	-m --alignments - \
+	--ref ../ref/Homo_sapiens.GRCh38.dna.primary_assembly.fa \
+	--output-name ../process/SRR23538291/SRR23538291_filtered_multi.tsv \
+	--base-change C,T &
+	
+	wait'
+
+
+# You should also comment this command when vtune, and run it after finishing analysing the work
+run_with_timing 'python bin/join_pileup.py \
+	-i ../process/SRR23538291/SRR23538291_unfiltered_uniq.tsv \
+	../process/SRR23538291/SRR23538291_unfiltered_multi.tsv \
+	../process/SRR23538291/SRR23538291_filtered_uniq.tsv \
+	../process/SRR23538291/SRR23538291_filtered_multi.tsv \
+	-o ../process/SRR23538291/SRR23538291_genome.arrow'
