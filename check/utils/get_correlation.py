a44da3a9fcf98a7ceda50ced347b937cc7e8b568
lshAlgorithm <lishuhuai_brain@163.com>
2025-02-11 00:35:12 +0800

    fix a test bug

--- check/utils/get_correlation.py
+++ check/utils/get_correlation.py
@@ -8,7 +8,8 @@ def match_and_extract(file1, file2, output_file):
     df1 = pd.read_csv(file1, sep='\t')
     df2 = pd.read_csv(file2, sep='\t')
     df1.rename(columns={'ratio': 'ur', 'chromosome': 'ref', 'position': 'pos'}, inplace=True)
-
+    df1['ref'] = df1['ref'].astype(str)
+    df2['ref'] = df2['ref'].astype(str)
     df1['pos'] = df1['pos'].astype(str)  # 将 pos 列转换为字符串
     df2['pos'] = df2['pos'].astype(str)  # 将 pos 列转换为字符串
     # 合并两个数据框，基于前三列（ref, pos, strand）
--- script/stage2.sh
+++ script/stage2.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 
-# python bin/group_pileup.py \
-#     -i ../process/SRR23538290/SRR23538290_genome.arrow \
-#     ../process/SRR23538291/SRR23538291_genome.arrow \
-#     ../process/SRR23538292/SRR23538292_genome.arrow \
-#     -o ../process/WT/WT.arrow
+python bin/group_pileup.py \
+     -i ../process/SRR23538290/SRR23538290_genome.arrow \
+     ../process/SRR23538291/SRR23538291_genome.arrow \
+     ../process/SRR23538292/SRR23538292_genome.arrow \
+     -o ../process/WT/WT.arrow
 
 python bin/select_sites.py -i ../process/WT/WT.arrow -o ../process/WT/WT.prefilter.tsv
 
