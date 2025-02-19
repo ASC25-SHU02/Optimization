18d6f9eb177abb1aea3f3d3a0b825ca5c682899e
Jonathan Zhang <admin@cestlavie.moe>
2025-02-19 20:13:35 +0800

    Update stage1_SRR23538292.sh

--- script/stage1_SRR23538292.sh
+++ script/stage1_SRR23538292.sh
@@ -3,21 +3,21 @@
 source ./script/func.sh
 
 # Comment this command when running vtune, run it separately before the workflow
-run_with_timing 'cutseq ../process/SRR23538292/SRR23538292.fastq \
+run_with_timing 'cutseq /asc25/process/SRR23538292/SRR23538292.fastq \
         -t 20 \
         -A INLINE \
         -m 20 \
         --trim-polyA \
         --ensure-inline-barcode\
-        -o ../process/SRR23538292/SRR23538292.fastq_cut \
-        -s ../process/SRR23538292/SRR23538292.fastq_tooshort \
-        -u ../process/SRR23538292/SRR23538292.fastq_untrimmed'
+        -o /asc25/process/SRR23538292/SRR23538292.fastq_cut \
+        -s /asc25/process/SRR23538292/SRR23538292.fastq_tooshort \
+        -u /asc25/process/SRR23538292/SRR23538292.fastq_untrimmed'
 
-run_with_timing 'hisat-3n --index ../ncrna_ref/Homo_sapiens.GRCh38.ncrna.fa \
-    --summary-file ../process/SRR23538292/map2ncrna.output.summary \
+run_with_timing 'hisat-3n --index /asc25/ncrna_ref/Homo_sapiens.GRCh38.ncrna.fa \
+    --summary-file /asc25/process/SRR23538292/map2ncrna.output.summary \
     --new-summary \
     -q \
-    -U ../process/SRR23538292/SRR23538292.fastq_cut\
+    -U /asc25/process/SRR23538292/SRR23538292.fastq_cut\
     -p 20 \
     --base-change C,T \
     --mp 8,2 \
@@ -27,22 +27,22 @@ run_with_timing 'hisat-3n --index ../ncrna_ref/Homo_sapiens.GRCh38.ncrna.fa \
     view -@ 20 \
     -e '!flag.unmap' \
     -O BAM \
-    -U ../process/SRR23538292/SRR23538292.ncrna.unmapped.bam \
-    -o ../process/SRR23538292/SRR23538292.ncrna.mapped.bam'
+    -U /asc25/process/SRR23538292/SRR23538292.ncrna.unmapped.bam \
+    -o /asc25/process/SRR23538292/SRR23538292.ncrna.mapped.bam'
 
 
 run_with_timing 'samtools fastq \
 	-@ 20 \
-	-O ../process/SRR23538292/SRR23538292.ncrna.unmapped.bam \
-	> ../process/SRR23538292/SRR23538292.mRNA.fastq'
+	-O /asc25/process/SRR23538292/SRR23538292.ncrna.unmapped.bam \
+	> /asc25/process/SRR23538292/SRR23538292.mRNA.fastq'
 
 run_with_timing 'hisat-3n \
-	--index ../ref/Homo_sapiens.GRCh38.dna.primary_assembly.fa \
+	--index /asc25/ref/Homo_sapiens.GRCh38.dna.primary_assembly.fa \
 	-p 20 \
-	--summary-file ../process/SRR23538292/map2genome.output.summary \
+	--summary-file /asc25/process/SRR23538292/map2genome.output.summary \
 	--new-summary \
 	-q \
-	-U ../process/SRR23538292/SRR23538292.mRNA.fastq \
+	-U /asc25/process/SRR23538292/SRR23538292.mRNA.fastq \
 	--directional-mapping \
 	--base-change C,T \
 	--pen-noncansplice 20 \
@@ -51,75 +51,75 @@ run_with_timing 'hisat-3n \
 	-@ 20 \
 	-e '!flag.unmap'\
        	-O BAM \
-	-U ../process/SRR23538292/SRR23538292.mRNA.genome.unmapped.bam \
-	-o ../process/SRR23538292/SRR23538292.mRNA.genome.mapped.bam'
+	-U /asc25/process/SRR23538292/SRR23538292.mRNA.genome.unmapped.bam \
+	-o /asc25/process/SRR23538292/SRR23538292.mRNA.genome.mapped.bam'
 
 
 run_with_timing 'samtools sort -@ 20 \
 	--write-index \
 	-O BAM \
-	-o ../process/SRR23538292/SRR23538292.mRNA.genome.mapped.sorted.bam \
-	../process/SRR23538292/SRR23538292.mRNA.genome.mapped.bam'
+	-o /asc25/process/SRR23538292/SRR23538292.mRNA.genome.mapped.sorted.bam \
+	/asc25/process/SRR23538292/SRR23538292.mRNA.genome.mapped.bam'
 
 
 run_with_timing 'samtools view \
 	-@ 20 \
 	-F 3980 \
-	-c ../process/SRR23538292/SRR23538292.mRNA.genome.mapped.sorted.bam \
-	> ../process/SRR23538292/SRR23538292.mRNA.genome.mapped.sorted.bam.tsv'
+	-c /asc25/process/SRR23538292/SRR23538292.mRNA.genome.mapped.sorted.bam \
+	> /asc25/process/SRR23538292/SRR23538292.mRNA.genome.mapped.sorted.bam.tsv'
 
 
 run_with_timing 'java -server -Xms8G -Xmx40G -Xss100M \
-	-Djava.io.tmpdir=../process/SRR23538292 \
+	-Djava.io.tmpdir=/asc25/process/SRR23538292 \
 	-jar /Tools/UMICollapse/umicollapse.jar bam \
 	-t 2 -T 20 --data naive --merge avgqual --two-pass \
-	-i ../process/SRR23538292/SRR23538292.mRNA.genome.mapped.sorted.bam \
-	-o ../process/SRR23538292/SRR23538292.mRNA.genome.mapped.sorted.dedup.bam \
-	> ../process/SRR23538292/SRR23538292.mRNA.genome.mapped.sorted.dedup.log'
+	-i /asc25/process/SRR23538292/SRR23538292.mRNA.genome.mapped.sorted.bam \
+	-o /asc25/process/SRR23538292/SRR23538292.mRNA.genome.mapped.sorted.dedup.bam \
+	> /asc25/process/SRR23538292/SRR23538292.mRNA.genome.mapped.sorted.dedup.log'
 
 
 run_with_timing 'samtools index \
 	-@ 20 \
-	../process/SRR23538292/SRR23538292.mRNA.genome.mapped.sorted.dedup.bam \
-	../process/SRR23538292/SRR23538292.mRNA.genome.mapped.sorted.dedup.bam.bai'
+	/asc25/process/SRR23538292/SRR23538292.mRNA.genome.mapped.sorted.dedup.bam \
+	/asc25/process/SRR23538292/SRR23538292.mRNA.genome.mapped.sorted.dedup.bam.bai'
 
 run_with_timing 'samtools view -@ 20 \
 	-e "[XM] * 20 <= (qlen-sclen) && [Zf] <= 3 && 3 * [Zf] <= [Zf] + [Yf]" \
-	../process/SRR23538292/SRR23538292.mRNA.genome.mapped.sorted.dedup.bam \
+	/asc25/process/SRR23538292/SRR23538292.mRNA.genome.mapped.sorted.dedup.bam \
 	-O BAM \
-	-o ../process/SRR23538292/SRR23538292.mRNA.genome.mapped.sorted.dedup.filtered.bam'
+	-o /asc25/process/SRR23538292/SRR23538292.mRNA.genome.mapped.sorted.dedup.filtered.bam'
 
 run_with_timing 'samtools view -e "rlen<100000" \
-	-h ../process/SRR23538292/SRR23538292.mRNA.genome.mapped.sorted.dedup.bam \
+	-h /asc25/process/SRR23538292/SRR23538292.mRNA.genome.mapped.sorted.dedup.bam \
 	| hisat-3n-table \
-	-u --alignments - --ref ../ref/Homo_sapiens.GRCh38.dna.primary_assembly.fa \
-	--output-name ../process/SRR23538292/SRR23538292_unfiltered_uniq.tsv \
+	-u --alignments - --ref /asc25/ref/Homo_sapiens.GRCh38.dna.primary_assembly.fa \
+	--output-name /asc25/process/SRR23538292/SRR23538292_unfiltered_uniq.tsv \
 	--base-change C,T &
 
 	samtools view -e "rlen<100000" \
-	-h ../process/SRR23538292/SRR23538292.mRNA.genome.mapped.sorted.dedup.bam \
+	-h /asc25/process/SRR23538292/SRR23538292.mRNA.genome.mapped.sorted.dedup.bam \
 	| hisat-3n-table \
 	-m --alignments - \
-	--ref ../ref/Homo_sapiens.GRCh38.dna.primary_assembly.fa \
-	--output-name ../process/SRR23538292/SRR23538292_unfiltered_multi.tsv \
+	--ref /asc25/ref/Homo_sapiens.GRCh38.dna.primary_assembly.fa \
+	--output-name /asc25/process/SRR23538292/SRR23538292_unfiltered_multi.tsv \
 	--base-change C,T &
 
 	samtools view \
 	-e "rlen<100000" \
-	-h ../process/SRR23538292/SRR23538292.mRNA.genome.mapped.sorted.dedup.filtered.bam \
+	-h /asc25/process/SRR23538292/SRR23538292.mRNA.genome.mapped.sorted.dedup.filtered.bam \
 	| hisat-3n-table \
 	-u --alignments - \
-	--ref ../ref/Homo_sapiens.GRCh38.dna.primary_assembly.fa \
-	--output-name ../process/SRR23538292/SRR23538292_filtered_uniq.tsv\
+	--ref /asc25/ref/Homo_sapiens.GRCh38.dna.primary_assembly.fa \
+	--output-name /asc25/process/SRR23538292/SRR23538292_filtered_uniq.tsv\
 	--base-change C,T &
 
     samtools view \
 	-e "rlen<100000" \
-	-h ../process/SRR23538292/SRR23538292.mRNA.genome.mapped.sorted.dedup.filtered.bam \
+	-h /asc25/process/SRR23538292/SRR23538292.mRNA.genome.mapped.sorted.dedup.filtered.bam \
 	| hisat-3n-table \
 	-m --alignments - \
-	--ref ../ref/Homo_sapiens.GRCh38.dna.primary_assembly.fa \
-	--output-name ../process/SRR23538292/SRR23538292_filtered_multi.tsv \
+	--ref /asc25/ref/Homo_sapiens.GRCh38.dna.primary_assembly.fa \
+	--output-name /asc25/process/SRR23538292/SRR23538292_filtered_multi.tsv \
 	--base-change C,T &
 	
 	wait'
@@ -127,8 +127,8 @@ run_with_timing 'samtools view -e "rlen<100000" \
 
 # You should also comment this command when vtune, and run it after finishing analysing the work
 run_with_timing 'python bin/join_pileup.py \
-	-i ../process/SRR23538292/SRR23538292_unfiltered_uniq.tsv \
-	../process/SRR23538292/SRR23538292_unfiltered_multi.tsv \
-	../process/SRR23538292/SRR23538292_filtered_uniq.tsv \
-	../process/SRR23538292/SRR23538292_filtered_multi.tsv \
-	-o ../process/SRR23538292/SRR23538292_genome.arrow'
+	-i /asc25/process/SRR23538292/SRR23538292_unfiltered_uniq.tsv \
+	/asc25/process/SRR23538292/SRR23538292_unfiltered_multi.tsv \
+	/asc25/process/SRR23538292/SRR23538292_filtered_uniq.tsv \
+	/asc25/process/SRR23538292/SRR23538292_filtered_multi.tsv \
+	-o /asc25/process/SRR23538292/SRR23538292_genome.arrow'
