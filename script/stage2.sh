ef187399630d0bb42bd138acfe834bcedf8f795a
Jonathan Zhang <admin@cestlavie.moe>
2025-02-19 20:15:33 +0800

    Update stage2.sh

--- script/stage2.sh
+++ script/stage2.sh
@@ -2,27 +2,27 @@
 
 
 python bin/group_pileup.py \
-     -i ../process/SRR23538290/SRR23538290_genome.arrow \
-     ../process/SRR23538291/SRR23538291_genome.arrow \
-     ../process/SRR23538292/SRR23538292_genome.arrow \
-     -o ../process/WT/WT.arrow
+     -i /asc25/process/SRR23538290/SRR23538290_genome.arrow \
+     /asc25/process/SRR23538291/SRR23538291_genome.arrow \
+     /asc25/process/SRR23538292/SRR23538292_genome.arrow \
+     -o /asc25/process/WT/WT.arrow
 
-python bin/select_sites.py -i ../process/WT/WT.arrow -o ../process/WT/WT.prefilter.tsv
-
-python bin/filter_sites.py \
-    -i ../process/SRR23538290/SRR23538290_genome.arrow \
-    -m ../process/WT/WT.prefilter.tsv \
-    -b ../process/SRR23538290/SRR23538290.bg.tsv \
-    -o ../process/SRR23538290/SRR23538290.filtered.tsv
+python bin/select_sites.py -i /asc25/process/WT/WT.arrow -o /asc25/process/WT/WT.prefilter.tsv
 
 python bin/filter_sites.py \
-    -i ../process/SRR23538291/SRR23538291_genome.arrow \
-    -m ../process/WT/WT.prefilter.tsv \
-    -b ../process/SRR23538291/SRR23538291.bg.tsv \
-    -o ../process/SRR23538291/SRR23538291.filtered.tsv
+    -i /asc25/process/SRR23538290/SRR23538290_genome.arrow \
+    -m /asc25/process/WT/WT.prefilter.tsv \
+    -b /asc25/process/SRR23538290/SRR23538290.bg.tsv \
+    -o /asc25/process/SRR23538290/SRR23538290.filtered.tsv
 
 python bin/filter_sites.py \
-    -i ../process/SRR23538292/SRR23538292_genome.arrow \
-    -m ../process/WT/WT.prefilter.tsv \
-    -b ../process/SRR23538292/SRR23538292.bg.tsv \
-    -o ../process/SRR23538292/SRR23538292.filtered.tsv
+    -i /asc25/process/SRR23538291/SRR23538291_genome.arrow \
+    -m /asc25/process/WT/WT.prefilter.tsv \
+    -b /asc25/process/SRR23538291/SRR23538291.bg.tsv \
+    -o /asc25/process/SRR23538291/SRR23538291.filtered.tsv
+
+python bin/filter_sites.py \
+    -i /asc25/process/SRR23538292/SRR23538292_genome.arrow \
+    -m /asc25/process/WT/WT.prefilter.tsv \
+    -b /asc25/process/SRR23538292/SRR23538292.bg.tsv \
+    -o /asc25/process/SRR23538292/SRR23538292.filtered.tsv
