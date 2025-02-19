d1a18b31766ff824600b4aa285bdc3fd0606e7b1
Jonathan Zhang <admin@cestlavie.moe>
2025-02-19 20:15:12 +0800

    Update pre.sh

--- script/pre.sh
+++ script/pre.sh
@@ -3,19 +3,19 @@
 hisat-3n-build \
 	-p 32 \
 	--base-change C,T \
-	../ref/Homo_sapiens.GRCh38.dna.primary_assembly.fa \
-	../ref/Homo_sapiens.GRCh38.dna.primary_assembly.fa
+	/asc25/ref/Homo_sapiens.GRCh38.dna.primary_assembly.fa \
+	/asc25/ref/Homo_sapiens.GRCh38.dna.primary_assembly.fa
 
-samtools faidx ../ref/Homo_sapiens.GRCh38.dna.primary_assembly.fa
+samtools faidx /asc25/ref/Homo_sapiens.GRCh38.dna.primary_assembly.fa
 
 awk 'BEGIN{{OFS="\\t"}}{{print $1,$1,0,$2,"+"}}' \
-	../ref/Homo_sapiens.GRCh38.dna.primary_assembly.fa.fai \
-	> ../ref/Homo_sapiens.GRCh38.dna.primary_assembly.fa.saf
+	/asc25/ref/Homo_sapiens.GRCh38.dna.primary_assembly.fa.fai \
+	> /asc25/ref/Homo_sapiens.GRCh38.dna.primary_assembly.fa.saf
 
 hisat-3n-build \
 	-p 16 \
 	--base-change C,T \
-	../ncrna_ref/Homo_sapiens.GRCh38.ncrna.fa \
-	../ncrna_ref/Homo_sapiens.GRCh38.ncrna.fa
+	/asc25/ncrna_ref/Homo_sapiens.GRCh38.ncrna.fa \
+	/asc25/ncrna_ref/Homo_sapiens.GRCh38.ncrna.fa
 
-samtools faidx ../ncrna_ref/Homo_sapiens.GRCh38.ncrna.fa
+samtools faidx /asc25/ncrna_ref/Homo_sapiens.GRCh38.ncrna.fa
